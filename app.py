fbf109d344f845be8e5055230f451ea51642d5d5
luiscarp <76863603+luiscarp@users.noreply.github.com>
2022-10-20 08:54:10 -0600

    cambio de imagen

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Mi aplicación")
 st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Plano.png")
+st.sidebar.image("http://fcq.uach.mx/images/institucionales/Historia/logo-escuela-quimica.png")
 boton = st.button("globos")
 if boton:
   st.balloons()
